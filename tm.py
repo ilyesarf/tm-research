3bb5ced54caed2eff248e7dc449d886d2701834a
Quimzy <bam.boy2020@gmail.com>
2023-08-08 20:13:54 +0300

    tm: it works!

--- tm.py
+++ tm.py
@@ -10,6 +10,26 @@ class Machine:
         self.halt = halt
         self.tape = tape
 
+    def eval_state(self, state, ind):
+        if ind > len(self.tape)-1:
+            self.tape = np.append(self.tape, 0)
+        elif ind == -1:
+            self.tape = np.insert(self.tape, 0, 0)
+            ind = 0
+        elif ind < -1:
+            print(ind)
+            exit(1)
+
+        symb_st = [st for st in self.states[state] if st.symb1 == self.tape[ind]][0] #symbol-specific state
+        self.tape[ind] = symb_st.symb2
+        
+        if symb_st.move == 'L':
+            ind -= 1
+        elif symb_st.move == 'R':
+            ind += 1
+
+        return symb_st.next_q, ind
+
 
 class State:
     def __init__(self, symb1, symb2, move, next_q):
@@ -27,7 +47,22 @@ if __name__ == '__main__':
     states = parser.parse_states()
     init_q = 'A'
     halt = 'H'
-    tape = np.array([])
+    tape = np.array([0])
 
     machine = Machine(alphabet, blank, states, init_q, halt, tape)
-    
+
+    state, ind = init_q, 0
+    i = 0
+    while True:
+        print("state: ", state)
+        print("head position: ", ind)
+        print("current tape: ", machine.tape)
+        print()
+        #print(i)
+        if state != 'H':
+            state,ind = machine.eval_state(state, ind)
+            i += 1
+        else:
+            print("total steps: ", i)
+            print("sum: ", np.sum(machine.tape))
+            exit(0)
