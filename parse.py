d0ee859562aed57ff0bdc044140508e9bfdcdc86
Quimzy <bam.boy2020@gmail.com>
2023-08-09 11:39:11 +0300

    parser: cleaner state parsing

--- parse.py
+++ parse.py
@@ -7,10 +7,10 @@ class Parser:
         from tm import State
         states = {}
         for st in self.unparsed_states:
-            st = st.strip().split(' ')
+            st = eval(st)
             if st[0] not in states.keys():
                 states[st[0]] = []
-                
-            states[st[0]].append(State(*[int(x) if x in "0123456789" else str(x) for x in st[1:]]))
+
+            states[st[0]].append(State(*st[1:]))
         
         return states
