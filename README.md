b24a1c40fc13f884f37ce863f2f9f45cc70ade87
Quimzy <bam.boy2020@gmail.com>
2023-09-03 18:25:12 +0300

    readme now in markdown & perf report added

---
+++ README.md
@@ -0,0 +1,12 @@
+Playing around with theoretical computing and Turing Machines
+
+## Perf
+
+### <a href="https://github.com/ilyesarf/tm-research/blob/master/massif.out.1705953"> massif report (3x2 busy beaver machine) </a>
+```
+time=242774
+mem_heap_B=1168
+mem_heap_extra_B=104
+mem_stacks_B=0
+heap_tree=peak
+```
--- bb/3x2.c
+++ bb/3x2.c
@@ -1,32 +1,41 @@
 #include "../tm.h"
 
+#define DEBUG 1
+
 int main(){
-    /*const char* unparsedStates[]={
-        "A 0 1 1 B",
-        "A 1 1 -1 C",
-        "B 0 1 -1 A",
-        "B 1 1 1 B",
-        "C 0 1 -1 B",
-        "C 1 1 1 H"
-    };*/
-
-    const char* unparsedStates[]={
-        "A 0 1 1 B",
-        "B 0 1 -1 C",   
-    };
-
-
-    char* remainingStates[] = {
-        "C 1 1 -1 B"
-    };
-
-    //runTape(l, unparsedStates);
-    
-    int l = 2; //len of current array of unpased states (variable)
-
-    while (l <= (sizeof(unparsedStates)+sizeof(remainingStates))/sizeof(unparsedStates[0])){
+    #ifdef DEBUG
+        const char* unparsedStates[]={
+            "A 0 1 1 B",
+            "A 1 1 -1 C",
+            "B 0 1 -1 A",
+            "B 1 1 1 B",
+            "C 0 1 -1 B",
+            "C 1 1 1 H"
+        };
+        
+        int l = 6; 
         runTape(l, unparsedStates);
-        unparsedStates[l] = remainingStates[(sizeof(unparsedStates)/sizeof(unparsedStates[0]))-l];
-        ++l;
-    }
+    #endif
+
+    #ifndef DEBUG
+        const char* unparsedStates[]={
+            "A 0 1 1 B",
+            "B 0 1 -1 C",   
+        };
+
+
+        char* remainingStates[] = {
+            "C 1 1 -1 B"
+        };
+
+        //runTape(l, unparsedStates);
+        
+        int l = 2; //len of current array of unpased states (variable)
+
+        while (l <= (sizeof(unparsedStates)+sizeof(remainingStates))/sizeof(unparsedStates[0])){
+            runTape(l, unparsedStates);
+            unparsedStates[l] = remainingStates[(sizeof(unparsedStates)/sizeof(unparsedStates[0]))-l];
+            ++l;
+        }
+    #endif
 }
